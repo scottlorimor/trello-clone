ff0bc0d397cfc37a222f7f52749ff666306d3efb
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-10 12:51:45 -0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,30 @@
-var app = angular.module('trelloClone', ['ui.router']);
+// Code here for your app.js to test the endpoint
 
-app.config(function ($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-        .state('auth', {
-            url: '/',
-            controller: 'AuthCtrl',
-            templateUrl: '../views/authView.html'
-        })
-        .state('todo', {
-            url: '/list',
-            controller: 'MainCtrl',
-            templateUrl: '../views/mainView.html',
-            resolve: {
-                lists: function (listService) {
-                    return listService.getLists();
-                }
-            }
-        })
-})
+
+
+// When you are done testing your own endpoint, comment out or erase your code, and use the code from below to test the trello app
+
+
+// var app = angular.module('trelloClone', ['ui.router']);
+
+// app.config(function ($urlRouterProvider, $stateProvider) {
+//     $urlRouterProvider.otherwise('/');
+
+//     $stateProvider
+//         .state('auth', {
+//             url: '/',
+//             controller: 'AuthCtrl',
+//             templateUrl: '../views/authView.html'
+//         })
+//         .state('todo', {
+//             url: '/list',
+//             controller: 'MainCtrl',
+//             templateUrl: '../views/mainView.html',
+//             resolve: {
+//                 lists: function (listService) {
+//                     return listService.getLists();
+//                 }
+//             }
+//         })
+// })
