f2b95e48e6a175cfa0c74760568f329d063cfeae
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-10 12:54:17 -0700

    Update MainCtrl.js

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,9 @@
-angular
-    .module('trelloClone')
-    .controller('MainCtrl', function ($scope, lists) {
+// When you are done testing your own endpoint, use the code from below to test the trello app
 
-    $scope.lists = lists;
+// angular
+//     .module('trelloClone')
+//     .controller('MainCtrl', function ($scope, lists) {
 
-});
+//     $scope.lists = lists;
+
+// });
