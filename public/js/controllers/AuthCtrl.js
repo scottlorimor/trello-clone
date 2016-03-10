397d8b861d84b9d4dd61ddeea6e02a68c29ef525
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-10 12:53:45 -0700

    Update AuthCtrl.js

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -1,33 +1,40 @@
-angular
-    .module('trelloClone')
-    .controller('AuthCtrl', function ($scope, $state, authService) {
+// Code here for your authCtrl.js to test the endpoint
 
-        $scope.test = 'auth';
 
-        $scope.handleEnterOnInput = function (event) {
-            if (event.keyCode === 13) {
-                $scope.login();
-            }
-        };
 
-        $scope.login = function () {
-            authService.login($scope.username)
-                .then(function (response) {
-                    $state.go('todo')
-                })
-                .catch(function (err) {
-                    console.log(err);
-                })
-        };
 
-        $scope.logout = function () {
-            authService.logout()
-                .then(function (response) {
-                    $state.go('auth')
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    $state.go('auth');
-                })
-        }
-});
+// When you are done testing your own endpoint, comment out or erase your code, and use the code from below to test the trello app
+
+// angular
+//     .module('trelloClone')
+//     .controller('AuthCtrl', function ($scope, $state, authService) {
+
+//         $scope.test = 'auth';
+
+//         $scope.handleEnterOnInput = function (event) {
+//             if (event.keyCode === 13) {
+//                 $scope.login();
+//             }
+//         };
+
+//         $scope.login = function () {
+//             authService.login($scope.username)
+//                 .then(function (response) {
+//                     $state.go('todo')
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                 })
+//         };
+
+//         $scope.logout = function () {
+//             authService.logout()
+//                 .then(function (response) {
+//                     $state.go('auth')
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                     $state.go('auth');
+//                 })
+//         }
+// });
