45655c10a18632f5fbf7ca53a1a06823317a7b49
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-10 12:53:09 -0700

    Update authservice.js

--- public/js/services/authservice.js
+++ public/js/services/authservice.js
@@ -1,33 +1,35 @@
-angular
-    .module('trelloClone')
-    .service('authService', function ($http, $q) {
-        this.login = function (username) {
-            var dfd = $q.defer();
-            $http.post('/auth/login', {
-                    username: username
-                })
-                .then(function (response) {
-                    dfd.resolve(response.data)
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    dfd.reject(err)
-                });
+// When you are done testing your own endpoint, use the code from below to test the trello app
 
-            return dfd.promise;
-        };
+// angular
+//     .module('trelloClone')
+//     .service('authService', function ($http, $q) {
+//         this.login = function (username) {
+//             var dfd = $q.defer();
+//             $http.post('/auth/login', {
+//                     username: username
+//                 })
+//                 .then(function (response) {
+//                     dfd.resolve(response.data)
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                     dfd.reject(err)
+//                 });
 
-        this.logout = function () {
-            var dfd = $q.defer();
-            $http.post('/auth/logout')
-                .then(function (response) {
-                    dfd.resolve(response.data)
-                })
-                .catch(function (err) {
-                    dfd.reject(err)
-                });
+//             return dfd.promise;
+//         };
 
-            return dfd.promise;
+//         this.logout = function () {
+//             var dfd = $q.defer();
+//             $http.post('/auth/logout')
+//                 .then(function (response) {
+//                     dfd.resolve(response.data)
+//                 })
+//                 .catch(function (err) {
+//                     dfd.reject(err)
+//                 });
 
-        }
-});
+//             return dfd.promise;
+
+//         }
+// });
