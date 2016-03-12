d563c686e48fa6159b58172c950d4de773bd9b25
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-12 07:54:57 -0700

    Update authservice.js

--- public/js/services/authservice.js
+++ public/js/services/authservice.js
@@ -1,33 +1,43 @@
-angular
-    .module('trelloClone')
-    .service('authService', function ($http, $q) {
-        this.login = function (username) {
-            var dfd = $q.defer();
-            $http.post('/auth/login', {
-                    username: username
-                })
-                .then(function (response) {
-                    dfd.resolve(response.data)
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    dfd.reject(err)
-                });
+// Build out your authService here. You will need to make a call to your server and send a body of data with the username,
+// then whatever your server returns needs to be returned to the controller.
 
-            return dfd.promise;
-        };
 
-        this.logout = function () {
-            var dfd = $q.defer();
-            $http.post('/auth/logout')
-                .then(function (response) {
-                    dfd.resolve(response.data)
-                })
-                .catch(function (err) {
-                    dfd.reject(err)
-                });
 
-            return dfd.promise;
 
-        }
-});
+
+
+
+
+// angular
+//     .module('trelloClone')
+//     .service('authService', function ($http, $q) {
+//         this.login = function (username) {
+//             var dfd = $q.defer();
+//             $http.post('/auth/login', {
+//                     username: username
+//                 })
+//                 .then(function (response) {
+//                     dfd.resolve(response.data)
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                     dfd.reject(err)
+//                 });
+
+//             return dfd.promise;
+//         };
+
+//         this.logout = function () {
+//             var dfd = $q.defer();
+//             $http.post('/auth/logout')
+//                 .then(function (response) {
+//                     dfd.resolve(response.data)
+//                 })
+//                 .catch(function (err) {
+//                     dfd.reject(err)
+//                 });
+
+//             return dfd.promise;
+
+//         }
+// });
