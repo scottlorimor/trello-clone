aec4e9b432147dc8be41fcf9048b989f58ababd7
Dustin Myers <dustinmyers@users.noreply.github.com>
2016-03-11 22:18:35 -0700

    Update AuthCtrl.js

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -1,33 +1,42 @@
-angular
-    .module('trelloClone')
-    .controller('AuthCtrl', function ($scope, $state, authService) {
+// Code your AuthCtrl here following the project guide - When you have put the commented out code back into your server.js, use the code from below to 
+// connect the login function to the backend, and then retest your app.
 
-        $scope.test = 'auth';
 
-        $scope.handleEnterOnInput = function (event) {
-            if (event.keyCode === 13) {
-                $scope.login();
-            }
-        };
 
-        $scope.login = function () {
-            authService.login($scope.username)
-                .then(function (response) {
-                    $state.go('todo')
-                })
-                .catch(function (err) {
-                    console.log(err);
-                })
-        };
 
-        $scope.logout = function () {
-            authService.logout()
-                .then(function (response) {
-                    $state.go('auth')
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    $state.go('auth');
-                })
-        }
-});
+
+
+
+// angular
+//     .module('trelloClone')
+//     .controller('AuthCtrl', function ($scope, $state, authService) {
+
+//         $scope.test = 'auth';
+
+//         $scope.handleEnterOnInput = function (event) {
+//             if (event.keyCode === 13) {
+//                 $scope.login();
+//             }
+//         };
+
+//         $scope.login = function () {
+//             authService.login($scope.username)
+//                 .then(function (response) {
+//                     $state.go('todo')
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                 })
+//         };
+
+//         $scope.logout = function () {
+//             authService.logout()
+//                 .then(function (response) {
+//                     $state.go('auth')
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                     $state.go('auth');
+//                 })
+//         }
+// });
